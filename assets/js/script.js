2f96d9799e486d2d12f8e1ea7f4e8b8eca548f84
Justin Schleicher <IT.Schleicher@gmail.com>
2024-10-10 17:57:23 -0500

    working customer name and make Pizza buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,7 +51,7 @@ let anchovies = {
 
 
 // creating variables and listeners for customer name button    
-const customerInput = document.getElementById('customerInput')
+let customerInput = document.getElementById('customerInput')
 
 const nameButton = document.getElementById('namesubmitButton')
 
@@ -59,7 +59,7 @@ let customerName = "Customer"
 
 nameButton.addEventListener('click', function() {
     
-    let customerName = customerInput.value;
+    customerName = customerInput.value;
     console.log(customerName)
 });
 
@@ -67,20 +67,23 @@ nameButton.addEventListener('click', function() {
 
 let toppings = [pepperoni,sausage,greenpepper,mushroom,blackolives,onions,grilledchicken,pineapple,bacon,basil,garlic,anchovies]
 
-//make list of toppings on pizza by checking for if onpizza is true
-const toppinglist = toppings.filter(toppings => toppings.onpizza)
 
-// pulling only the toppinglist.text attribute from the array
-const toppingtextonly = toppinglist.map(toppinglist => toppinglist.text)
+const completeOrder = document.getElementById('completeOrder');
 
-// creates final string of toppings
-const toppingstext = toppingtextonly.join(", ");
 
-// concacting name of customer with pizza toppings into final output
-const pizzaoutput  = "Hello, " + customerName + "! Your pizza has " + toppingstext + " on it.  Sounds Delicious!"
 
-//set toppingstext to an ID that can be called in HTML
-document.getElementById("pizzaresult").textContent = pizzaoutput;
+completeOrder.addEventListener('click', function() {
+    const toppinglist = toppings.filter(toppings => toppings.onpizza)
+    const toppingtextonly = toppinglist.map(toppinglist => toppinglist.text)
+    const toppingstext = toppingtextonly.join(", ");
+    console.log(customerName)
+    let pizzaoutput  = "Hello, " + customerName + "! Your pizza has " + toppingstext + " on it.  Sounds Delicious!"
+    console.log(pizzaoutput)
+    document.getElementById("pizzaresult").textContent = pizzaoutput;
+});
+
+
+
 
 function myFunction() {
     var element = document.body;
